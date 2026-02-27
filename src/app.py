ae8f8a13dcf8a5993868606acac06ba11e87cde8
j031nich0145 <j031nich0145@gmail.com>
2026-02-26 18:28:27 -0800

    bar_charts+++

--- src/app.py
+++ src/app.py
@@ -395,7 +395,7 @@ def server(input, output, session):
         ax.set_title(f"Economic Loss by Disaster Type ({stat.capitalize()})")
         ax.set_yticklabels([format_currency(v) for v in ax.get_yticks()]) 
         plt.subplots_adjust(bottom=0.25, left=0.15) 
-        plt.yticks(rotation=45, ha="right")
+        plt.xticks(rotation=45, ha="right")
         plt.tight_layout()
         return fig
 
@@ -411,7 +411,7 @@ def server(input, output, session):
         ax.set_ylabel("Economic Aid (USD)")
         ax.set_title(f"Economic Aid by Disaster Type ({stat.capitalize()})")
         ax.set_yticklabels([format_currency(v) for v in ax.get_yticks()])  
-        plt.yticks(rotation=45, ha="right")
+        plt.xticks(rotation=45, ha="right")
         plt.subplots_adjust(bottom=0.25, left=0.15) 
         plt.tight_layout()
         return fig
